54c067daf6076a913a9bdffa214645d0618eb8a7
Florian Mielke <github@initialwert.de>
2023-03-03 11:29:25 +0100

    We now can add multiple errors to an error message

--- Sources/Mailer/Message.swift
+++ Sources/Mailer/Message.swift
@@ -7,37 +7,39 @@
 import UIKit
 
 public struct Message: Equatable, Hashable {
-    public var subject = ""
-    public var body = ""
-    public var isHTML: Bool = false
-    public var toRecipients: [String]
-    public var ccRecipients = [String]()
-    public var bccRecipients = [String]()
-    public var attachments = [Attachment]()
+  public var subject = ""
+  public var body = ""
+  public var isHTML: Bool = false
+  public var toRecipients: [String]
+  public var ccRecipients = [String]()
+  public var bccRecipients = [String]()
+  public var attachments = [Attachment]()
 }
 
 // MARK: - Error
 extension Message {
-    public init() {
-        toRecipients = Mailer.Configuration.shared.defaultRecipients
-        body = Mailer.Configuration.shared.appendsDeviceInformation ? Message.deviceInformation : ""
-    }
-    
-    public init(error: Error) {
-        self.init()
-        if let logFile = Attachment(error: error) {
-            attachments = [logFile]
-        }
-    }
-
-    public static var support: Message {
-        var message = Message()
-        message.subject = "\(Bundle.main.appName) \(Bundle.main.presentableVersionNumber)"
-        return message
-    }
-    
-    private static var deviceInformation: String {
-        let footer =
+  public init() {
+    toRecipients = Mailer.Configuration.shared.defaultRecipients
+    body = Mailer.Configuration.shared.appendsDeviceInformation ? Message.deviceInformation : ""
+  }
+  
+  public init(error: Error) {
+    self.init(errors: [error])
+  }
+  
+  public init(errors: [Error]) {
+    self.init()
+    attachments = errors.compactMap { Attachment(error: $0) }
+  }
+  
+  public static var support: Message {
+    var message = Message()
+    message.subject = "\(Bundle.main.appName) \(Bundle.main.presentableVersionNumber)"
+    return message
+  }
+  
+  private static var deviceInformation: String {
+    let footer =
             """
             
             -------------------------
@@ -45,7 +47,7 @@ extension Message {
             iOS Device: \(UIDevice.current.localizedModel)
             System Language: \(Locale.preferredLanguages.first ?? "Unknown")
             """
-        
-        return footer
-    }
+    
+    return footer
+  }
 }
